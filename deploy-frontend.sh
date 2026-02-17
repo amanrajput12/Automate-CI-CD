337f9a10510bc3938a7813da776f95d28a4dbccd
Aman Rajput <rajputaman724857@gmail.com>
2026-02-17 22:33:03 +0530

    fix error

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/Automate-CI-CD
 git pull
 npm run test
+rm -f dist/ --recursive
 npm run build
 aws s3 rm s3://storageapp-aman --recursive
-rm -f dist/ --recursive
 aws s3 cp dist/ s3://storageapp-aman --recursive
